eb0e4612fc16e9b4fa6f0a2a06f1abc09f867edc
Jesusalan0102 <jesus.alan.sepulveda.soto82@uabc.edu.mx>
2026-05-21 11:57:20 -0700

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/bas/app_83fd3b1b-5d1d-43fd-be37-63f56db0efe8/backend
-exec /home/bas/venv/bin/uvicorn main:app --host 0.0.0.0 --port $PORT
+exec /home/bas/venv/bin/uvicorn main:app --host 0.0.0.0 --port 9000
