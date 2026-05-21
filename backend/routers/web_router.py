f3f034ea03476cd1d944b3cac7576901227ee6c3
Jesusalan0102 <jesus.alan.sepulveda.soto82@uabc.edu.mx>
2026-05-21 11:33:57 -0700

    Update web_router.py

--- backend/routers/web_router.py
+++ backend/routers/web_router.py
@@ -1,7 +1,9 @@
 
 from fastapi.responses import HTMLResponse
 from fastapi import APIRouter, Request, Depends, HTTPException
-
+from routers.auth_router import verify_token_cookie
+# o desde un archivo común de autenticación
+from utils.auth import verify_token_cookie
 router = APIRouter()
 
 # ------------------------------------------------------------
