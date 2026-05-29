7f8443fd7e5003693e65a736b9283b97272a1bb0
Jesusalan0102 <jesus.alan.sepulveda.soto82@uabc.edu.mx>
2026-05-29 13:34:18 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -51,7 +51,7 @@ app.include_router(ws_router)
 app.include_router(cluster_router)
 app.include_router(web_router)
 app.include_router(asistencia_api_router)
-app.include_router(horarios_api_router)
+
 
 @app.get("/")
 def root():
