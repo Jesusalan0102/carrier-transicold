c9d77aadb9ca9b42884696fdf5d284f516fed54d
Jesusalan0102 <jesus.alan.sepulveda.soto82@uabc.edu.mx>
2026-05-29 11:38:59 -0700

    Update __init__.py

--- backend/asistencia/__init__.py
+++ backend/asistencia/__init__.py
@@ -1,5 +1,5 @@
-from backend.asistencia import routes
-from backend.asistencia.horarios_routes import router as horarios_router
-from backend.asistencia.templates import get_checkin_template, ASISTENCIA_STYLES
+from asistencia import routes
+from asistencia.horarios_routes import router as horarios_router
+from asistencia.templates import get_checkin_template, ASISTENCIA_STYLES
 
 __all__ = ["routes", "horarios_router", "get_checkin_template", "ASISTENCIA_STYLES"]
