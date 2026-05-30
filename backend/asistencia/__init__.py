9270725ddd68017a1933d88721bd951370bf5cf8
Jesusalan0102 <jesus.alan.sepulveda.soto82@uabc.edu.mx>
2026-05-29 20:53:27 -0700

    Delete backend/asistencia/__init__.py

--- backend/asistencia/__init__.py
+++
@@ -1,5 +0,0 @@
-from asistencia import routes
-from asistencia.horarios_routes import router as horarios_router
-from asistencia.templates import get_checkin_template, ASISTENCIA_STYLES
-
-__all__ = ["routes", "horarios_router", "get_checkin_template", "ASISTENCIA_STYLES"]
